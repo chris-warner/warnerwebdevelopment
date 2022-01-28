d7eed356b3d8995f4e66dde27fd85eda908530d2
Chris Warner <cwarner@concentrichx.com>
2022-01-28 12:45:29 -0500

    Updates

---
+++ precache-manifest.e9667a1dbb5dc3b659eca17afaf0332e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bd763cd1dad296a7c7459a31bfda2d",
+    "url": "/warnerwebdevelopment/index.html"
+  },
+  {
+    "revision": "33e78d879e47399531b2",
+    "url": "/warnerwebdevelopment/static/css/main.96fb9f8a.chunk.css"
+  },
+  {
+    "revision": "3cf178a297d348d101ff",
+    "url": "/warnerwebdevelopment/static/js/2.9a539c6d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/warnerwebdevelopment/static/js/2.9a539c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e78d879e47399531b2",
+    "url": "/warnerwebdevelopment/static/js/main.0f672670.chunk.js"
+  },
+  {
+    "revision": "ffa5429728cf5a289414",
+    "url": "/warnerwebdevelopment/static/js/runtime-main.4d05b425.js"
+  },
+  {
+    "revision": "c6e3e51833a4d3bbeb3812ef6e4e3734",
+    "url": "/warnerwebdevelopment/static/media/3d_space_warp.c6e3e518.png"
+  },
+  {
+    "revision": "015e2ef33dd57994166b7fbcf53f1b79",
+    "url": "/warnerwebdevelopment/static/media/FriendFinder.015e2ef3.png"
+  },
+  {
+    "revision": "bb595b093f705d1cc2a897378dc65dc9",
+    "url": "/warnerwebdevelopment/static/media/List Products.bb595b09.png"
+  },
+  {
+    "revision": "86a19a787189c4d69c41cd96440b8603",
+    "url": "/warnerwebdevelopment/static/media/MindStock.86a19a78.png"
+  },
+  {
+    "revision": "4d03373f39204e2715d2e33c6ac6341e",
+    "url": "/warnerwebdevelopment/static/media/buddy.4d03373f.png"
+  },
+  {
+    "revision": "93db960a47ed08f4dc0c6e9b6d033923",
+    "url": "/warnerwebdevelopment/static/media/burger.93db960a.png"
+  },
+  {
+    "revision": "10c5ff600e6f4ad4f233df209b170052",
+    "url": "/warnerwebdevelopment/static/media/clicky-game.10c5ff60.png"
+  },
+  {
+    "revision": "5c43f23619af65ed475403ce54d016a0",
+    "url": "/warnerwebdevelopment/static/media/fugatch.5c43f236.png"
+  },
+  {
+    "revision": "f9bc5d337e28e64e0666889d4d8d9826",
+    "url": "/warnerwebdevelopment/static/media/gsap_banner_ad.f9bc5d33.png"
+  },
+  {
+    "revision": "5a147418d33fc2b7c46b04381df53665",
+    "url": "/warnerwebdevelopment/static/media/hero.5a147418.jpg"
+  },
+  {
+    "revision": "98100702ac1dcbaf903b27798d38d58d",
+    "url": "/warnerwebdevelopment/static/media/liri.98100702.png"
+  },
+  {
+    "revision": "1a2191fe9ae291393c395e634a63541a",
+    "url": "/warnerwebdevelopment/static/media/me.1a2191fe.png"
+  },
+  {
+    "revision": "4846905a57fb9df88ee36c65fb555236",
+    "url": "/warnerwebdevelopment/static/media/technics-q-c-640-480-1.4846905a.png"
+  },
+  {
+    "revision": "a23aca6274f833d2d7f6249c41cc8ec2",
+    "url": "/warnerwebdevelopment/static/media/technics-q-c-640-480-2.a23aca62.png"
+  },
+  {
+    "revision": "4be6da38b5f7cfbc3c00e81a6a872bdd",
+    "url": "/warnerwebdevelopment/static/media/technics-q-c-640-480-5.4be6da38.png"
+  },
+  {
+    "revision": "10be37d463505113cdaad9e5f22a5f3f",
+    "url": "/warnerwebdevelopment/static/media/technics-q-c-640-480-7.10be37d4.png"
+  },
+  {
+    "revision": "2723472065ff601c7c8d81f30c7a9772",
+    "url": "/warnerwebdevelopment/static/media/tip.27234720.png"
+  }
+]);
